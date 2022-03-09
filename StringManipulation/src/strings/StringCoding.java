16b8976750ed16653514f42511be51e1df4ca7c7
Corey Stacy <stacy.corey1992@gmail.com>
2022-03-09 18:13:54 -0500

    Added Occurrences class

--- StringManipulation/src/strings/StringCoding.java
+++ StringManipulation/src/strings/StringCoding.java
@@ -7,24 +7,27 @@ public class StringCoding {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		//Consonant counter
-		Consonants.countConsonants(s);
+//		//Consonant counter
+//		Consonants.countConsonants(s);
+//		
+//		//Vowel counter
+//		Vowels.vowelCounter(s);
+//		
+//		//Palindrome checker
+//		Palindrome.isPalindrome(s);
+//		
+//		//Word counter
+//		Words.numWords(s);
+//		
+//		//Rotate String
+//		RotateString.rotate(s);
+//
+//		//Count words ending in 's' and 'y'
+//		//Switch first 's' word with first 'y' word
+//		LastLetterCount.countAndSwitch(s);
 		
-		//Vowel counter
-		Vowels.vowelCounter(s);
-		
-		//Palindrome checker
-		Palindrome.isPalindrome(s);
-		
-		//Word counter
-		Words.numWords(s);
-		
-		//Rotate String
-		RotateString.rotate(s);
-
-		//Count words ending in 's' and 'y'
-		//Switch first 's' word with first 'y' word
-		LastLetterCount.countAndSwitch(s);
+		//Count number of occurrences
+		Occurrences.countOccurrence();
 
 	}
 
